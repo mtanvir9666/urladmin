e14039b4e84abe9f4b9dbdd4f8cdf712e699c587
Tanvir Alam <tanviralam@Tanvirs-MacBook-Pro.local>
2021-05-28 22:38:50 -0700

    Project skeleton

---
+++ src/main/java/com/tinyurl/urladmin/UrladminApplication.java
@@ -0,0 +1,13 @@
+package com.tinyurl.urladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrladminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrladminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinyurl/urladmin/UrladminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyurl.urladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrladminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
